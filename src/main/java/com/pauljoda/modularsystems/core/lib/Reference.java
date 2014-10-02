0194b364529fc90f26370c7467d75cb8b3c1fe9b
pauljoda <pauldavis101@gmail.com>
2014-10-02 16:59:06 -0500

    Update

--- src/main/java/com/pauljoda/modularsystems/core/lib/Reference.java
+++ src/main/java/com/pauljoda/modularsystems/core/lib/Reference.java
@@ -18,7 +18,7 @@ public class Reference {
 	
 	public static final String MOD_ID = "modularsystems";
 	public static final String MOD_NAME = "Modular Systems";
-	public static final String VERSION = "1.43";
+	public static final String VERSION = "1.44";
 	public static final String CHANNEL_NAME = MOD_ID;
 
 	//FURNACE: Blocks that are from my mod, used to prevent overlaying on reload
