9d6bb0527e27284ecb5d48862dea6bd1eae1213e
Dyonovan <mcbetacraft@gmail.com>
2015-07-08 23:40:31 -0400

    Fixed Register Blocks

--- src/main/java/com/pauljoda/modularsystems/core/managers/BlockManager.java
+++ src/main/java/com/pauljoda/modularsystems/core/managers/BlockManager.java
@@ -15,9 +15,9 @@ public class BlockManager {
     public static Block furnaceCore, furnaceCoreActive, furnaceDummy;
 
     public static void init() {
-        registerBlock(furnaceCore = new BlockFurnaceCore(false), Reference.MOD_ID + ":furnaceCore", TileEntityFurnaceCore.class);
-        registerBlock(furnaceCoreActive = new BlockFurnaceCore(true), Reference.MOD_ID + ":furnaceCoreActive", TileEntityFurnaceCore.class);
-        registerBlock(furnaceDummy = new BlockFurnaceDummy(), Reference.MOD_ID + ":furnaceDummy", TileEntityFurnaceDummy.class);
+        registerBlock(furnaceCore = new BlockFurnaceCore(false), "furnaceCore", TileEntityFurnaceCore.class);
+        registerBlock(furnaceCoreActive = new BlockFurnaceCore(true), "furnaceCoreActive", TileEntityFurnaceCore.class);
+        registerBlock(furnaceDummy = new BlockFurnaceDummy(), "furnaceDummy", TileEntityFurnaceDummy.class);
     }
 
     public static void registerBlock(Block block, String name, Class<? extends TileEntity> tileEntity, String oreDict) {
