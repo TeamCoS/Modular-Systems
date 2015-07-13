9568a2e6c943aae9929c5da046864b427a1ec32f
Paul Davis <pauldavis101@gmail.com>
2015-07-13 17:15:07 -0500

    New gui

--- src/main/java/com/pauljoda/modularsystems/power/gui/GuiLiquidsPower.java
+++ src/main/java/com/pauljoda/modularsystems/power/gui/GuiLiquidsPower.java
@@ -3,11 +3,12 @@ package com.pauljoda.modularsystems.power.gui;
 import com.pauljoda.modularsystems.power.container.ContainerLiquidsPower;
 import com.pauljoda.modularsystems.power.tiles.TileLiquidsPower;
 import com.teambr.bookshelf.client.gui.component.display.GuiComponentArrow;
-import com.teambr.bookshelf.client.gui.component.display.GuiComponentPowerBar;
+import com.teambr.bookshelf.client.gui.component.display.GuiComponentFluidTank;
+import com.teambr.bookshelf.helpers.GuiHelper;
 import net.minecraft.entity.player.InventoryPlayer;
 
-import java.awt.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class GuiLiquidsPower extends GuiPowerBase<ContainerLiquidsPower> {
 
@@ -17,32 +18,28 @@ public class GuiLiquidsPower extends GuiPowerBase<ContainerLiquidsPower> {
         super(new ContainerLiquidsPower(inventory, tileEntity), tileEntity, 200, 165, "inventory.liquidspower.title");
 
         this.tileEntity = tileEntity;
+        addComponents();
     }
 
     @Override
     public void addComponents() {
-        components.add(new GuiComponentPowerBar(19, 18, 18, 60, new Color(255, 0, 0)) {
-            @Override
-            public int getEnergyPercent(int scale) {
-                return tileEntity.tank == null ? 0 : tileEntity.tank.getFluidAmount() * scale / tileEntity.tank.getCapacity();
-            }
-        });
+        if(tileEntity != null) {
+            components.add(new GuiComponentFluidTank(19, 18, 18, 60, tileEntity.tank) {
+                @Override
+                public List<String> getDynamicToolTip(int mouseX, int mouseY) {
+                    ArrayList<String> toolTipLiquid = new ArrayList<>();
+                    toolTipLiquid.add(tileEntity.tank.getFluid() != null ? GuiHelper.GuiColor.YELLOW + tileEntity.tank.getFluid().getLocalizedName() : "Empty");
+                    toolTipLiquid.add(tileEntity.tank.getFluidAmount() + " / " + tileEntity.tank.getCapacity() + "mb");
+                    return toolTipLiquid;
+                }
+            });
 
-        components.add(new GuiComponentArrow(84, 40) {
-            @Override
-            public int getCurrentProgress() {
-                return 0;
-            }
-        });
-    }
-
-    @Override
-    public void drawGuiContainerForegroundLayer(int x, int y) {
-        super.drawGuiContainerForegroundLayer(x, y);
-
-        ArrayList<String> toolTipLiquid = new ArrayList<>();
-        toolTipLiquid.add(tileEntity.tank.getFluidAmount() + " / " + tileEntity.tank.getCapacity());
-        toolTipLiquid.add(tileEntity.tank.getFluid() != null ? tileEntity.tank.getFluid().getLocalizedName() : "Empty");
-        components.get(1).setToolTip(toolTipLiquid);
+            components.add(new GuiComponentArrow(84, 40) {
+                @Override
+                public int getCurrentProgress() {
+                    return 0;
+                }
+            });
+        }
     }
 }
