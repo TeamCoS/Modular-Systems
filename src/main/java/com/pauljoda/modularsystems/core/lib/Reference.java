754a83a443bc024a71b3505a1c33ec656d3c5310
Paul Davis <pauldavis101@gmail.com>
2015-07-15 22:04:04 -0500

    Fix

--- src/main/java/com/pauljoda/modularsystems/core/lib/Reference.java
+++ src/main/java/com/pauljoda/modularsystems/core/lib/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
     public static final String MOD_NAME = "Modular Systems";
     public static final String MOD_ID = "modularsystems";
     public static final String VERSION = "@VERSION@";
-    public static final String DEPENDENCIES = "required-after:bookshelf";
+    public static final String DEPENDENCIES = "required-after:bookshelf@[1.0,)";
 
     public static final String MULTIBLOCK_CONFIG = "MultiBlock Options";
     public static final String DEBUG = "Debug Mode";
