b4c5d46de71b1332e38f0ce8252d0cd1c22f1577
Dyonovan <mcbetacraft@gmail.com>
2015-07-20 18:27:59 -0400

    Fixing server client sync

--- src/main/java/com/pauljoda/modularsystems/core/managers/FMLEventManager.java
+++ src/main/java/com/pauljoda/modularsystems/core/managers/FMLEventManager.java
@@ -3,12 +3,12 @@ package com.pauljoda.modularsystems.core.managers;
 import com.pauljoda.modularsystems.core.network.GetHashPacket;
 import com.pauljoda.modularsystems.core.registries.BlockValueRegistry;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.common.gameevent.PlayerEvent;
+import cpw.mods.fml.common.network.FMLNetworkEvent;
 
 public class FMLEventManager {
 
     @SubscribeEvent
-    public void onClientConnectEvent(PlayerEvent.PlayerLoggedInEvent event) {
+    public void onClientConnectEvent(FMLNetworkEvent.ClientConnectedToServerEvent event) {
         int hash = BlockValueRegistry.INSTANCE.materialValues.hashCode();
         PacketManager.net.sendToServer(new GetHashPacket.HashMessage(hash));
     }
