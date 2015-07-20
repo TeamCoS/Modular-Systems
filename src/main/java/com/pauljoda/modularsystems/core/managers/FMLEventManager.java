0a07fcb85009d78406dee09fc3b5621eb6608ca8
Paul Davis <pauldavis101@gmail.com>
2015-07-20 18:43:06 -0500

    Work around

--- src/main/java/com/pauljoda/modularsystems/core/managers/FMLEventManager.java
+++ src/main/java/com/pauljoda/modularsystems/core/managers/FMLEventManager.java
@@ -2,14 +2,13 @@ package com.pauljoda.modularsystems.core.managers;
 
 import com.pauljoda.modularsystems.core.collections.BlockValues;
 import com.pauljoda.modularsystems.core.network.AddCalculationPacket;
-import com.pauljoda.modularsystems.core.network.GetHashPacket;
 import com.pauljoda.modularsystems.core.registries.BlockValueRegistry;
 import com.teambr.bookshelf.collections.Couplet;
 import com.teambr.bookshelf.helpers.BlockHelper;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.network.FMLNetworkEvent;
 import net.minecraft.block.Block;
-import net.minecraft.client.Minecraft;
+import net.minecraft.network.NetHandlerPlayServer;
 
 import java.util.Map;
 
@@ -17,13 +16,12 @@ public class FMLEventManager {
 
     @SubscribeEvent
     public void onClientConnectEvent(FMLNetworkEvent.ServerConnectionFromClientEvent event) {
-
         for (Map.Entry<String, BlockValues> entry : BlockValueRegistry.INSTANCE.values.entrySet()){
             Couplet<Block, Integer> block = BlockHelper.getBlockFromString(entry.getKey());
             BlockValues value = entry.getValue();
             PacketManager.net.sendTo(new AddCalculationPacket.CalculationMessage(block.getFirst(), block.getSecond(),
                             value.getSpeedFunction(), value.getEfficiencyFunction(), value.getMultiplicityFunction()),
-                    event.handler.);
+                    ((NetHandlerPlayServer)event.handler).playerEntity);
         }
     }
 }
