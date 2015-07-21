759f580c8972608d71e339f9779d4b71b3815695
Dyonovan <mcbetacraft@gmail.com>
2015-07-21 02:31:27 -0400

    Fixed Waila showing its own version of RF Energy

--- src/main/java/com/pauljoda/modularsystems/core/proxy/CommonProxy.java
+++ src/main/java/com/pauljoda/modularsystems/core/proxy/CommonProxy.java
@@ -1,9 +1,12 @@
 package com.pauljoda.modularsystems.core.proxy;
 
+import cpw.mods.fml.common.event.FMLInterModComms;
+
 public class CommonProxy {
     public void preInit() {}
 
     public void init() {
-
+        FMLInterModComms.sendMessage("Waila", "register",
+                "com.teambr.bookshelf.api.waila.WailaDataProvider.callbackRegisterServer");
     }
 }
