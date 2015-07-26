5148db06ad777856585c1d85bf146aa7d58e1e5a
Paul Davis <pauldavis101@gmail.com>
2015-07-26 00:07:24 -0500

    Stuff

--- src/main/java/com/pauljoda/modularsystems/storage/gui/GuiComponentEditableList.java
+++ src/main/java/com/pauljoda/modularsystems/storage/gui/GuiComponentEditableList.java
@@ -10,6 +10,7 @@ import com.teambr.bookshelf.util.RenderUtils;
 import net.minecraft.util.StatCollector;
 import org.lwjgl.opengl.GL11;
 
+import java.awt.*;
 import java.util.List;
 
 /**
@@ -18,7 +19,7 @@ import java.util.List;
  */
 public abstract class GuiComponentEditableList<T> extends BaseComponent {
 
-    protected int width = 120;
+    protected int width = 135;
     protected int height = 120;
 
     protected List<T> list;
@@ -45,16 +46,25 @@ public abstract class GuiComponentEditableList<T> extends BaseComponent {
      */
     public abstract void valueSaved(List<T> listToSaveTo, String valuePassed);
 
+    /**
+     * This is where you should convert the object into what you want to display
+     *
+     * toString() is not always what you want, so here you can convert things
+     * @param object The object to convert to string
+     * @return What you want to display
+     */
+    public abstract String convertObjectToString(T object);
+
     @Override
     public void initialize() {
         renderer = new NinePatchRenderer(0, 80, 3);
 
-        inputBox = new GuiComponentTextBox(0, 0, 100, 16) {
+        inputBox = new GuiComponentTextBox(xPos, yPos, 100, 16) {
             @Override
             public void fieldUpdated(String value) {}
         };
 
-        saveButton = new GuiComponentButton(105, 0, 15, 16, StatCollector.translateToLocal("inventory.blockValuesConfig.save")) {
+        saveButton = new GuiComponentButton(xPos + 105, yPos - 3, 30, 20, StatCollector.translateToLocal("inventory.blockValuesConfig.save")) {
             @Override
             public void doAction() {
                 valueSaved(list, inputBox.getValue());
@@ -63,7 +73,7 @@ public abstract class GuiComponentEditableList<T> extends BaseComponent {
             }
         };
 
-        scrollBar = new GuiComponentScrollBar(110, 20, 100) {
+        scrollBar = new GuiComponentScrollBar(xPos + 121, yPos + 20, 100) {
             @Override
             public void onScroll(float position) {
                 currentPosition = (int)position * list.size();
@@ -71,15 +81,66 @@ public abstract class GuiComponentEditableList<T> extends BaseComponent {
         };
     }
 
+    /**
+     * Called when the mouse is pressed
+     * @param mouseX Mouse X Position
+     * @param mouseY Mouse Y Position
+     * @param button Mouse Button
+     */
+    public void mouseDown(int mouseX, int mouseY, int button) {
+        inputBox.mouseDown(mouseX, mouseY, button);
+        if(scrollBar.isMouseOver(mouseX, mouseY))
+            scrollBar.mouseDown(mouseX, mouseY, button);
+        if(saveButton.isMouseOver(mouseX, mouseY))
+            saveButton.mouseDown(mouseX, mouseY, button);
+    }
+
+    /**
+     * Called when the user drags the component
+     * @param x Mouse X Position
+     * @param y Mouse Y Position
+     * @param button Mouse Button
+     * @param time How long
+     */
+    public void mouseDrag(int x, int y, int button, long time) {
+        inputBox.mouseDrag(x, y, button, time);
+        if(scrollBar.isMouseOver(x, y))
+            scrollBar.mouseDrag(x, y, button, time);
+        if(saveButton.isMouseOver(x, y))
+            saveButton.mouseDrag(x, y, button, time);
+    }
+
+    /**
+     * Called when the mouse button is over the component and released
+     * @param x Mouse X Position
+     * @param y Mouse Y Position
+     * @param button Mouse Button
+     */
+    public void mouseUp(int x, int y, int button) {
+        inputBox.mouseUp(x, y, button);
+        if(scrollBar.isMouseOver(x, y))
+            scrollBar.mouseUp(x, y, button);
+        if(saveButton.isMouseOver(x, y))
+            saveButton.mouseUp(x, y, button);
+    }
+
+    /**
+     * Used when a key is pressed
+     * @param letter The letter
+     * @param keyCode The code
+     */
+    public void keyTyped(char letter, int keyCode) {
+        inputBox.keyTyped(letter, keyCode);
+    }
+
     @Override
     public void render(int guiLeft, int guiTop) {
         GL11.glPushMatrix();
 
-        GL11.glTranslated(xPos, yPos, 0);
         RenderUtils.bindGuiComponentsSheet();
         RenderUtils.prepareRenderState();
 
-        renderer.render(this, 0, 20, 100, 100);
+        renderer.render(this, xPos -1, yPos + 20, 120, 100, new Color(100, 100, 100));
         inputBox.render(guiLeft, guiTop);
         saveButton.render(guiLeft, guiTop);
         scrollBar.render(guiLeft, guiTop);
@@ -89,7 +150,19 @@ public abstract class GuiComponentEditableList<T> extends BaseComponent {
     }
 
     @Override
-    public void renderOverlay(int guiLeft, int guiTop) {}
+    public void renderOverlay(int guiLeft, int guiTop) {
+        GL11.glPushMatrix();
+
+        RenderUtils.bindGuiComponentsSheet();
+        RenderUtils.prepareRenderState();
+
+        inputBox.renderOverlay(guiLeft, guiTop);
+        saveButton.renderOverlay(guiLeft, guiTop);
+        scrollBar.renderOverlay(guiLeft, guiTop);
+
+        RenderUtils.restoreRenderState();
+        GL11.glPopMatrix();
+    }
 
     @Override
     public int getWidth() {
@@ -99,5 +172,13 @@ public abstract class GuiComponentEditableList<T> extends BaseComponent {
     @Override
     public int getHeight() {
         return height;
+    }
+
+    public GuiComponentScrollBar getScrollBar() {
+        return scrollBar;
+    }
+
+    public GuiComponentTextBox getInputBox() {
+        return inputBox;
     }
 }
--- src/main/java/com/pauljoda/modularsystems/storage/gui/GuiStorageSecurity.java
+++ src/main/java/com/pauljoda/modularsystems/storage/gui/GuiStorageSecurity.java
@@ -5,7 +5,12 @@ import com.teambr.bookshelf.client.gui.GuiBase;
 import com.teambr.bookshelf.client.gui.component.control.GuiComponentCheckBox;
 import com.teambr.bookshelf.inventory.ContainerGeneric;
 import com.teambr.bookshelf.manager.PacketManager;
+import net.minecraft.client.gui.GuiPlayerInfo;
+import net.minecraft.client.network.NetHandlerPlayClient;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,9 +19,13 @@ import java.util.List;
  */
 public class GuiStorageSecurity extends GuiBase<ContainerGeneric> {
     protected TileStorageSecurity securityTile;
+    protected float currentScroll;
+    List<String> matchingPlayers = new ArrayList<>();
+    int position = 0;
+    protected GuiComponentEditableList<String> editField;
 
     public GuiStorageSecurity(TileStorageSecurity tile) {
-        super(new ContainerGeneric(), 175, 150, "tile.modularsystems:storageSecurity.name");
+        super(new ContainerGeneric(), 175, 175, "tile.modularsystems:storageSecurity.name");
         securityTile = tile;
         addComponents();
     }
@@ -32,12 +41,84 @@ public class GuiStorageSecurity extends GuiBase<ContainerGeneric> {
                 }
             });
 
-            components.add(new GuiComponentEditableList<String>(5, 40, securityTile.getCore().getAllowedPlayers()) {
+            components.add(editField = new GuiComponentEditableList<String>(20, 40, securityTile.getCore().getAllowedPlayers()) {
                 @Override
                 public void valueSaved(List<String> listToSaveTo, String valuePassed) {
 
                 }
+
+                @Override
+                public String convertObjectToString(String object) {
+                    return null;
+                }
             });
+        }
+    }
+
+    @Override
+    public void handleMouseInput() {
+        super.handleMouseInput();
+        int scrollDirection = Mouse.getEventDWheel();
+        if (scrollDirection != 0 && securityTile.getCore().getAllowedPlayers().size() > 5) {
+
+            int rowsAboveDisplay = securityTile.getCore().getAllowedPlayers().size() - 5;
+            if (scrollDirection > 0)
+                scrollDirection = 1;
+            else
+                scrollDirection = -1;
+
+            this.currentScroll = (float) ((double) this.currentScroll - (double) scrollDirection / (double) rowsAboveDisplay);
+
+            if (currentScroll < 0.0F)
+                currentScroll = 0.0F;
+            else if (currentScroll > 1.0F)
+                currentScroll = 1.0F;
+
+            this.editField.scrollBar.setPosition(this.currentScroll);
+            updateScreen();
+        }
+        if(this.editField.getInputBox() != null && this.editField.getInputBox().getTextField() != null && this.editField.getInputBox().getTextField().isFocused())
+            Keyboard.enableRepeatEvents(true);
+        else
+            Keyboard.enableRepeatEvents(false);
+    }
+
+    @Override
+    protected void keyTyped(char letter, int keyCode) {
+        if(keyCode == 15) {
+            completePlayers();
+        }
+        else {
+            matchingPlayers.clear();
+        }
+        if(this.editField.getInputBox() != null && this.editField.getInputBox().getTextField() != null && !this.editField.getInputBox().getTextField().textboxKeyTyped(letter, keyCode)) {
+            super.keyTyped(letter, keyCode);
+        }
+    }
+
+    public void completePlayers() {
+        String starting;
+        if(this.editField.getInputBox().getValue() != null) {
+            starting = this.editField.getInputBox().getValue();
+            if ((!this.mc.isIntegratedServerRunning() || this.mc.thePlayer.sendQueue.playerInfoList.size() > 1)) {
+                this.mc.mcProfiler.startSection("playerList");
+                NetHandlerPlayClient netclienthandler = this.mc.thePlayer.sendQueue;
+                List list = netclienthandler.playerInfoList;
+
+                for (Object aList : list) {
+                    GuiPlayerInfo guiplayerinfo = (GuiPlayerInfo) aList;
+                    if (guiplayerinfo.name.toLowerCase().contains(starting.toLowerCase())) {
+                        matchingPlayers.add(guiplayerinfo.name);
+                    }
+                }
+            }
+        }
+
+        if(matchingPlayers.size() > 0) {
+            ++position;
+            if(position >= matchingPlayers.size())
+                position = 0;
+            this.editField.getInputBox().getTextField().setText(matchingPlayers.get(position));
         }
     }
 }
