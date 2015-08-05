25852718bf991afb80b09a999449e523fb591f02
Paul Davis <pauldavis101@gmail.com>
2015-08-05 16:58:53 -0500

    Storage Core

--- src/main/scala/com/teambr/modularsystems/storage/container/ContainerStorageCore.scala
+++ src/main/scala/com/teambr/modularsystems/storage/container/ContainerStorageCore.scala
@@ -1,10 +1,10 @@
 package com.teambr.modularsystems.storage.container
 
+import java.util
+
 import com.teambr.bookshelf.common.container.BaseContainer
 import com.teambr.modularsystems.storage.tiles.TileStorageCore
-import net.minecraft.inventory.{IInventory, Slot}
-
-import scala.collection.mutable.ListBuffer
+import net.minecraft.inventory.{ InventoryCraftResult, InventoryCrafting, IInventory, Slot }
 
 /**
  * Modular-Systems
@@ -13,16 +13,44 @@ import scala.collection.mutable.ListBuffer
 class ContainerStorageCore(playerInventory: IInventory, val storageCore: TileStorageCore)
         extends BaseContainer(playerInventory, storageCore) {
 
-   // var displaySlots = new Lists.newArrayList;
-    var allSlots = new ListBuffer[Slot]
-    var currentSearch: String = ""
+    var displaySlots = new util.ArrayList[Slot]
+    displaySlots.addAll(inventorySlots[_ <: Slot])
+    var allSlots = new util.ArrayList[Slot]
+    allSlots.addAll(displaySlots)
+    var currentSearch = ""
 
-    {
-        addInventoryGrid(25, 27, 11)
-        //displaySlots.addAll(inventorySlots)
-       // allSlots.addAll(displaySlots)
-        addPlayerInventorySlots(44, 140)
+    var craftingGrid : InventoryCrafting = null
+    var craftResult = new InventoryCraftResult
 
+    addInventoryGrid(25, 27, 11, 6)
+
+    if(storageCore.getInventoryRowCount > 6) {
+        var slotId = inventorySlots.size()
+        for(i <- 66 until storageCore.getSizeInventory()) {
+            addSlotToContainer(new RestrictedSlot(inventory, slotId, -10000, -10000))
+            slotId += 1
+        }
     }
 
+    addPlayerInventorySlots(if(storageCore.hasCraftingUpgrade) 8 else 44, 140)
+
+    if(storageCore.hasCraftingUpgrade) {
+        craftingGrid = new DummyCraftingInventory(storageCore, this)
+    }
+
+
+    private def addInventoryGrid(xOffset : Int, yOffset : Int, width : Int, rows : Int) : Unit = {
+        var slotId = 0
+        for(y <- 0 until rows) {
+            for(x <- 0 until width) {
+                addSlotToContainer(new RestrictedSlot(inventory, slotId, xOffset + x * 18, yOffset + y * 18))
+                slotId += 1
+            }
+        }
+    }
+
+    class DummyCraftingInventory(tile : TileStorageCore, container : ContainerStorageCore)
+            extends InventoryCrafting(null, 3, 3) {
+
+    }
 }
