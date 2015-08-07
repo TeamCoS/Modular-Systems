eba62006ab400132239d98903fb21c1b125ede92
Paul Davis <pauldavis101@gmail.com>
2015-08-07 15:18:53 -0500

    Why didn't this send

--- src/main/scala/com/teambr/modularsystems/furnace/tiles/TileEntityFurnaceCore.scala
+++ src/main/scala/com/teambr/modularsystems/furnace/tiles/TileEntityFurnaceCore.scala
@@ -2,15 +2,12 @@ package com.teambr.modularsystems.furnace.tiles
 
 import com.teambr.modularsystems.core.blocks.BlockProxy
 import com.teambr.modularsystems.core.functions.BlockCountFunction
-import com.teambr.modularsystems.core.managers.BlockManager
 import com.teambr.modularsystems.core.registries.FurnaceBannedBlocks
 import com.teambr.modularsystems.core.tiles.AbstractCore
 import net.minecraft.block.Block
 import net.minecraft.inventory.Container
 import net.minecraft.item.ItemStack
 import net.minecraft.item.crafting.FurnaceRecipes
-import net.minecraft.util.BlockPos
-import net.minecraft.world.World
 
 /**
  * This file was created for Modular-Systems
@@ -25,28 +22,12 @@ import net.minecraft.world.World
 class TileEntityFurnaceCore extends AbstractCore {
 
     /**
-     * Used to set the blocks to its active and non-active state
-     * @param positiveBurnTime True if active
-     * @param world World object
-     * @param pos The block position
-     */
-    override def updateBlockState(positiveBurnTime: Boolean, world: World, pos: BlockPos): Unit = {
-
-    }
-
-    /**
      * Take the blocks in this structure and generate the speed etc values
      * @param function The blocks count function
      */
     override def generateValues(function: BlockCountFunction): Unit = {
 
     }
-
-    /**
-     * Gets around that last little bit where the values line up weird and it doesn't convert back
-     * @return The block to set when on
-     */
-    override def getOnBlock: Block = BlockManager.furnaceCoreActive
 
     /**
      * Check if this blocks is not allowed in the structure
