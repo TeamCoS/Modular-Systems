eb9a5474a5ee4772f204a538a4df8dc236adbe7e
Dyonovan <mcbetacraft@gmail.com>
2015-08-07 21:04:15 -0400

    Fixes

--- src/main/scala/com/teambr/modularsystems/core/common/blocks/BlockProxy.scala
+++ src/main/scala/com/teambr/modularsystems/core/common/blocks/BlockProxy.scala
@@ -1,11 +1,14 @@
 package com.teambr.modularsystems.core.common.blocks
 
-import com.teambr.modularsystems.core.tiles.TileProxy
+import com.teambr.bookshelf.Bookshelf
+import com.teambr.modularsystems.core.tiles.{AbstractCore, TileProxy}
 import net.minecraft.block.material.Material
 import net.minecraft.block.properties.IProperty
 import net.minecraft.block.state.{BlockState, IBlockState}
-import net.minecraft.util.{BlockPos, EnumWorldBlockLayer}
-import net.minecraft.world.IBlockAccess
+import net.minecraft.creativetab.CreativeTabs
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.util.{EnumFacing, BlockPos, EnumWorldBlockLayer}
+import net.minecraft.world.{IBlockAccess, World}
 import net.minecraftforge.common.property.{ExtendedBlockState, IUnlistedProperty}
 import net.minecraftforge.fml.relauncher.{Side, SideOnly}
 
@@ -20,6 +23,30 @@ import net.minecraftforge.fml.relauncher.{Side, SideOnly}
  * @since August 06, 2015
  */
 class BlockProxy extends BaseBlock(Material.rock, "proxy", classOf[TileProxy]) {
+
+    override def getCreativeTab: CreativeTabs = {
+        null
+    }
+
+    override def breakBlock(world: World, pos: BlockPos, state: IBlockState) {
+        world.getTileEntity(pos) match {
+            case tile: TileProxy =>
+                if (tile.getCore.isDefined) {
+                    world.getTileEntity(tile.getCore.get.getPos).asInstanceOf[AbstractCore].deconstructMultiblock()
+                }
+            case _ =>
+        }
+    }
+
+    override def onBlockActivated(world : World, pos : BlockPos, state : IBlockState, player : EntityPlayer, side : EnumFacing, hitX : Float, hitY : Float, hitZ : Float) : Boolean = {
+        world.getTileEntity(pos) match {
+            case tile: TileProxy =>
+                if (tile.getCore.isDefined) {
+                    player.openGui(Bookshelf, 0, world, tile.getCore.get.getPos.getX, tile.getCore.get.getPos.getY, tile.getCore.get.getPos.getZ)
+                }
+        }
+        true
+    }
 
     override def createBlockState() : BlockState = {
         val listed = new Array[IProperty](0)
